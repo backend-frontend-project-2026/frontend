7aebaca4ab6ad4f6d9e188ea09f72eadd4800bb2
Viktoria <viktoria220506@gmail.com>
2026-03-14 20:29:06 +0300

    добавили +несколько setup router and page stubs

--- src/app/router/config/routesConfig.tsx
+++ src/app/router/config/routesConfig.tsx
@@ -5,25 +5,77 @@ import LandingPage from '../../../pages/landing/ui/LandingPage';
 import LoginPage from '../../../pages/auth/login/ui/LoginPage';
 import RegisterPage from '../../../pages/auth/register/ui/RegisterPage';
 
+import DiscoverPage from '../../../pages/discover/ui/DiscoverPage';
+import MatchesPage from '../../../pages/matches/ui/MatchesPage';
+import ChatsPage from '../../../pages/chats/ui/ChatsPage';
+import ProfilePage from '../../../pages/profile/ui/ProfilePage';
+import SettingsPage from '../../../pages/settings/ui/SettingsPage';
+
+import NotFoundPage from '../../../pages/not-found/ui/NotFoundPage';
+
 export const routesConfig = [
   {
     path: RoutePaths.LANDING,
     element: <LandingPage />,
   },
+
   {
     path: RoutePaths.LOGIN,
     element: <LoginPage />,
   },
+
   {
     path: RoutePaths.REGISTER,
     element: <RegisterPage />,
   },
+
   {
     path: RoutePaths.DISCOVER,
     element: (
-      <ProtectedRoute isAuth={false}>
+      <ProtectedRoute isAuth={true}>
         <DiscoverPage />
       </ProtectedRoute>
     ),
+  },
+
+  {
+    path: RoutePaths.MATCHES,
+    element: (
+      <ProtectedRoute isAuth={true}>
+        <MatchesPage />
+      </ProtectedRoute>
+    ),
+  },
+
+  {
+    path: RoutePaths.CHATS,
+    element: (
+      <ProtectedRoute isAuth={true}>
+        <ChatsPage />
+      </ProtectedRoute>
+    ),
+  },
+
+  {
+    path: RoutePaths.PROFILE,
+    element: (
+      <ProtectedRoute isAuth={true}>
+        <ProfilePage />
+      </ProtectedRoute>
+    ),
+  },
+
+  {
+    path: RoutePaths.SETTINGS,
+    element: (
+      <ProtectedRoute isAuth={true}>
+        <SettingsPage />
+      </ProtectedRoute>
+    ),
+  },
+
+  {
+    path: RoutePaths.NOT_FOUND,
+    element: <NotFoundPage />,
   },
 ];
